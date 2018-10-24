f911b9149fd68973972231b985b99803adf3b273
JON J <gr8fuldwg@gmail.com>
2018-10-23 20:55:41 -0400

    added click to cell and styled cell and alternated between X and O

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Its is working!!!');
+
+// variables
+const board = document.querySelector('.board');
+const cells = board.querySelectorAll('.cell');
+const turnDisplay = document.querySelector('.turn');
+const controls = document.querySelector('.control');
+let currentPlayer = 'X';
+let gameOver = false;
+
+// events
+cells.forEach(function attachCellListener(cell){
+    console.log(cell)
+    cell.addEventListener('click',  onCellClick); 
+});
+// functions
+function onCellClick(event) {
+    const elemtn =event.target;
+    elemtn.textContent = currentPlayer;
+    currentPlayer = currentPlayer === 'X' ? 'O': 'X';
+}
+// kick it off
